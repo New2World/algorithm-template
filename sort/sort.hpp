4f26f4d014bf5b7f9f12610a71ed4c319340a2df
Ritchie <ritchie_w@outlook.com>
2019-06-05 13:24:46 -0400

    fix bugs && rebuild

--- sort/sort.hpp
+++ sort/sort.hpp
@@ -1,137 +1,132 @@
 #pragma once
 
-int binarySearch(int *sort, int start, int end, int x)
-{
-	if(start == end || x == sort[(start+end)/2])
-		return start;
-	else
-	{
-		if(x > sort[(start+end)/2])
-			return binarySearch(sort,(start+end)/2+1,end,x);
-		else
-			return binarySearch(sort,start,(start+end)/2-1,x);
+#include <memory.h>
+
+#define H_PARENT(x) ((x-1)/2)
+#define H_LCHILD(x) (2*x+1)
+#define H_RCHILD(x) (2*x+2)
+
+void quickSort(int *arr, int n){
+	if(n <= 1)	return;
+	int *_head, *_end;
+	int key = arr[0];
+	_head = arr, _end = arr+n-1;
+	while(_head < _end){
+		while(key <= *_end && _head < _end) _end--;
+		*_head = *_end;
+		while(key >= *_head && _head < _end) _head++;
+		*_end = *_head;
 	}
-	return -1;
+	*_head = key;
+	quickSort(arr, _head-arr);
+	quickSort(_head+1, arr+n-_head-1);
 }
 
-void mergeSort(int *a, int *b, int start, int end)
-{
-	if(start >= end - 1)
-	{
-		if(a[start] > a[end])
-		{
-			int temp = a[end];
-			a[end] = a[start];
-			a[start] = temp;
-		}
-	}
-	else if(end - start > 1)
-	{
-		int half = start + (end - start) / 2;
-		mergeSort(a, b, start, half);
-		mergeSort(a, b, half + 1, end);
-		merge(a, b, start, half, end);
+void __adjustHeap(int *arr, int n, int id){
+	int minChild = H_LCHILD(id);
+	if(minChild >= n)	return;
+	if(H_RCHILD(id) < n && arr[H_RCHILD(id)] < arr[H_LCHILD(id)])
+		minChild = H_RCHILD(id);
+	if(arr[id] > arr[minChild]){
+		arr[id] ^= arr[minChild] ^= arr[id] ^= arr[minChild];
+		__adjustHeap(arr, n, minChild);
 	}
 }
 
-void merge(int *a, int *b, int start, int half, int end)
-{
-	int count = start;
-	int temp_start = start;
+void makeHeap(int *arr, int n){
+	if(n <= 1)	return;
+	for(int i = H_PARENT(n-1);i >= 0;i--)
+		__adjustHeap(arr, n, i);
+}
+
+void heapSort(int *arr, int n){
+	makeHeap(arr, n);
+	for(int i = 1;i < n;i++)
+		makeHeap(arr+i, n-i);
+}
+
+void merge(int *arr1, int *arr2, int head, int tail, int half){
+	int count = head;
+	int temp_start = head;
 	int temp_half = half;
-	while(count <= end)
-	{
-		if(temp_start <= half && temp_half <= end)
-			b[count++] = a[temp_start] < a[temp_half + 1] ? a[temp_start++] : a[temp_half++ + 1];
+	while(count <= tail){
+		if(temp_start <= half && temp_half <= tail)
+			arr2[count++] = arr1[temp_start] < arr1[temp_half+1] ? arr1[temp_start++] : arr1[temp_half++ + 1];
 		else if(temp_start > half)
-			b[count++] = a[temp_half++ +1];
-		else if(temp_half > end)
-			b[count++] = a[temp_start++];
+			arr2[count++] = arr1[temp_half++ +1];
+		else if(temp_half > tail)
+			arr2[count++] = arr1[temp_start++];
 	}
-	for(count = start;count <= end;count++)
-		a[count] = b[count];
+	for(count = head;count <= tail;count++)
+		arr1[count] = arr2[count];
 }
 
-void shellSort(int *arr, int k)
-{
-	int span = k/2;
-	while(span > 0)
-	{
-		for(int i = 0;i < span;i++)
-			insertSort(arr, i, k, span);
-		span /= 2;
-	}
-}
-
-void insertSort(int *arr, int from, int end, int span)
-{
-	int n = 1, i;
-	while(from+n*span < end)
-	{
-		i = n-1;
-		while(i >= 0 && arr[from+i*span] > arr[from+(i+1)*span])
-		{
-			arr[from+(i+1)*span] ^= arr[from+i*span] ^= arr[from+(i+1)*span] ^= arr[from+i*span];
-			i--;
-		}
-		n++;
-	}
-}
-
-void makeHeap(int *a, int x, int len)
-{
-	for(int i = x;2*i < len;)
-	{
-		if(a[i-1] > a[2*i-1])
-		{
-			if(a[2*i-1] > a[2*i])
-			{
-				a[i-1] ^= a[2*i] ^= a[i-1] ^= a[2*i];
-				i = 2*i+1;
-			}
-			else
-			{
-				a[i-1] ^= a[2*i-1] ^= a[i-1] ^= a[2*i-1];
-				i = 2*i;
-			}
-		}
-		else if(a[i-1] > a[2*i])
-		{
-			a[i-1] ^= a[2*i] ^= a[i-1] ^= a[2*i];
-			i = 2*i+1;
-		}
+void __merge(int *arr, int n, int mid){
+	int m_arr[n];
+	memcpy(m_arr, arr, sizeof(m_arr));
+	int p1 = 0, p2 = mid, p = 0;
+	while(p1 < mid && p2 < n){
+		if(m_arr[p1] <= m_arr[p2])
+			arr[p++] = m_arr[p1++];
 		else
-			i <<= 1;
+			arr[p++] = m_arr[p2++];
+	}
+	while(p1 < mid)
+		arr[p++] = m_arr[p1++];
+	while(p2 < n)
+		arr[p++] = m_arr[p2++];
+}
+
+void mergeSort(int *arr, int n){
+	if(n <= 1)	return;
+	if(n == 2){
+		if(arr[0] > arr[1])
+			arr[0] ^= arr[1] ^= arr[0] ^= arr[1];
+		return;
+	}
+	int mid = n>>1;
+	mergeSort(arr, mid);
+	mergeSort(arr+mid, n-mid);
+	__merge(arr, n, mid);
+}
+
+void insertSort(int *arr, int n, int d=1){
+	for(int i = d;i < n;i += d)
+		for(int j = i;j > 0;j -= d)
+			if(arr[j] < arr[j-d])
+				arr[j] ^= arr[j-d] ^= arr[j] ^= arr[j-d];
+			else	break;
+}
+
+void selectSort(int *arr, int n){
+	if(n <= 1)	return;
+	int ptr;
+	for(int i = 0;i < n;i++){
+		ptr = i;
+		for(int j = i+1;j < n;j++)
+			if(arr[ptr] > arr[j])
+				ptr = j;
+		if(ptr-i)
+			arr[i] ^= arr[ptr] ^= arr[i] ^= arr[ptr];
 	}
 }
 
-void heapSort(int *a, int len)
-{
-	for(int i = len/2;i > 0;i--)
-		makeHeap(a, i, len);
-	for(int i = 1;i < len-2;i++)
-		makeHeap(a+i, 1, len-i);
-	if(a[len-2] > a[len-1])
-		a[len-1] ^= a[len -2] ^= a[len-1] ^= a[len-2];
-}
-
-void quickSort(int *start, int *end)
-{
-	int *_Start, *_End;
-	int key = *start;
-	_Start = start, _End = end-1;
-	if(_Start > _End);
-	else
-	{
-		while(_Start < _End)
-		{
-			while(key < *_End && _Start < _End) _End--;
-			*_Start = *_End;
-			while(key > *_Start && _Start < _End) _Start++;
-			*_End = *_Start;
+void bubbleSort(int *arr, int n){
+	bool changed = true;
+	if(n <= 1)	return;
+	for(int j = 0;changed && j < n;j++){
+		changed = false;
+		for(int i = 1;i < n;i++){
+			if(arr[i-1] > arr[i]){
+				arr[i-1] ^= arr[i] ^= arr[i-1] ^= arr[i];
+				changed = true;
+			}
 		}
-		*_Start = key;
-		quickSort(start, _Start);
-		quickSort(_Start+1, end);
 	}
+}
+
+void shellSort(int *arr, int n){
+	for(int d = n;d;d >>= 1)
+		for(int i = 0;i < d;i++)
+			insertSort(arr+i, n-i, d);
 }
