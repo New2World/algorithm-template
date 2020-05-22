e88d00b8fd6587174f6bd33b94bb1cda69392486
Ritchie <New2World@users.noreply.github.com>
2020-05-22 22:32:51 +0800

    add annotations

--- graph/topology_sort.cpp
+++ graph/topology_sort.cpp
@@ -16,9 +16,9 @@ int ans[MAXLEN];
 
 void topologysort(int n){
     int ptr = 0, u, pre = -1;
-    priority_queue<int> q;
+    priority_queue<int> q;      // 因为题目要求要默认升序，在反向拓扑时就变成了降序
     for(int i = 1;i <= n;i++)
-        if(!neigh[i])
+        if(!neigh[i])           // 初始化，压入所有入度为 0 的点
             q.push(i);
     while(!q.empty()){
         u = q.top();
@@ -27,7 +27,7 @@ void topologysort(int n){
         pre = u;
         ans[ptr++] = u;
         for(int i = 0;i < graph[u].size();i++)
-            if(!--neigh[graph[u][i]])
+            if(!--neigh[graph[u][i]])       // 将邻接的点的入度减 1，如果减为 0 则压入队列
                 q.push(graph[u][i]);
     }
 }
@@ -43,8 +43,8 @@ int main(){
         scanf("%d %d", &n, &m);
         for(int i = 0;i < m;i++){
             scanf("%d %d", &a, &b);
-            graph[b].push_back(a);
-            neigh[a]++;
+            graph[b].push_back(a);          // 题目要求潜台词是没在输入的都得在后面，且默认升序
+            neigh[a]++;                     // 因此使用逆向拓扑，即没在输入的入度都为 0，先处理
         }
         topologysort(n);
         for(int i = n-1;i >= 0;i--)
